fa78270d5ba44d8deb23b7dea5aa92acfc46dce4
Harsh Vardhan <Harshvr003@gmail.com>
2026-06-06 22:51:27 +0530

    added live frontend url to cors origin

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ connectDB()
 
 app.use(express.json())
 app.use(cors({
-    origin : ["http://localhost:5173"],
+    origin : ["http://localhost:5173" , "https://3wassessment.netlify.app/"],
     methods : ["GET" , "POST" , "PUT" , "PATCH" , "DELETE"],
     credentials : true,
     optionsSuccessStatus : 200,
